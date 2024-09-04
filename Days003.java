3210dfdd38340e584156f66e7eecc509cdee526a
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-04 11:07:56 +0800

    class

---
+++ Days003.java
@@ -0,0 +1,11 @@
+
+package dasy003.java;
+public class Dasy003Java {
+    public static void main(String[] args) {
+        System.out.println("Nama : " + "Muhammad Fiqri");
+        System.out.println("Kelas : " + "Informatika D");
+        System.out.println("Alamat : ");
+        System.out.print("Bulubawang");
+    } 
+    
+}
