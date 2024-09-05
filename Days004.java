b94a7f4452f28793c340b16cefee727808f5fd42
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-05 12:03:55 +0800

    printf

--- Days004.java
+++ Days004.java
@@ -1,7 +1,7 @@
 package Day004;
 public class java {
     public static void main(String[] args) {
-        System.out.printf("halo semua perkenalkan:"); 
+        System.out.println("halo semua perkenalkan:"); 
         System.out.printf("nama saya %s dan saya berumur %d tahun.", "Muhammad Fiqri",19);
     }
 }
