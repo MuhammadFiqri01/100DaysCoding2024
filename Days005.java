24b1ae93c95fd74ef4465ec5fc0999db1f3dbc60
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-06 10:04:22 +0800

    komentar

---
+++ Days005.java
@@ -0,0 +1,27 @@
+
+package days005.java;
+
+public class Days005Java {
+
+    public static void main(String[] args) {
+        // ada dua tipe komentar yaitu :
+        /**/
+        //
+        
+        /*tipe pertama adalah tipe yang dapat berkomentar secara leluasa 
+        tanpa mengulang kode komentar tersebut
+        contoh 
+        nama : Muhammad fiqri
+        kelas : informatika D
+        nim : 000000
+        */
+        
+        //tipe kedua adalah tipe yang hanya bisa berkomentar satu baris
+        // contoh 
+        // nama : muhmmad fiqri
+        // kelas : Informatika D
+        // Nim : 0000000
+        
+    }
+    
+}
