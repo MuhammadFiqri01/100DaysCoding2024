3dddc00a2428bcd7ebf119ff819d0fa30ac2389b
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-07 09:52:43 +0800

    Variabel

---
+++ Days006.java
@@ -0,0 +1,67 @@
+
+package dasy006.java;
+
+public class Dasy006Java {
+
+    public static void main(String[] args) {
+        System.out.println("Contoh Tipe data variabel;");
+        // contoh tipe data nama_Variabel;
+        
+        int nomer;
+        
+        nomer = 12;
+        System.out.println("angka : " + nomer);
+        nomer = 13;
+        System.out.println("angka : " + nomer);
+        
+        byte satuan;
+        
+        satuan = 14;
+        System.out.println("nomer : " + satuan);
+        satuan = 15;
+        System.out.println("nomer : " + satuan);
+        
+        short bilangan;
+        
+        bilangan = 2005;
+        System.out.println("Tahun : " + bilangan);
+        bilangan = 2006;
+        System.out.println("Tahun : " + bilangan);
+        
+        long hitung;
+        
+        hitung = 16;
+        System.out.println("HITUNG : " + hitung);
+        hitung = 17;
+        System.out.println("HITUNG : " + hitung);
+        
+        float bilangan2 ;
+        
+        bilangan2 = (float) 1.70;
+        System.out.println("kg : " + bilangan2);
+        bilangan2 = (float) 2.60;
+        System.out.println("kg : " + bilangan2);
+        
+        double panjang2;
+        
+        panjang2 = 1.70;
+        System.out.println("tinggi : " + panjang2 + 'M');
+        panjang2 = 1.80;
+        System.out.println("tinggi : " + panjang2 + 'M');
+        
+        char variabel;
+        
+        variabel = 'S';
+        System.out.println("kapital : " + variabel);
+        variabel = 'L';
+        System.out.println("kapital : " + variabel);
+        
+        boolean sakit;
+        
+        sakit = true;
+        System.out.println("sakit : " + sakit);
+        sakit = false;
+        System.out.println("sakit : " + sakit);
+    }
+    
+}
