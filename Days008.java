21ee5f2600ee86c2499140d7d33f918c6d7917fa
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-09 07:33:35 +0800

    variabel update

---
+++ Days008.java
@@ -0,0 +1,57 @@
+
+package days008.java;
+
+public class Days008Java {
+
+    public static void main(String[] args) {
+     /*contoh dari variabel update*/  
+    
+    //sebelum di update
+        int nomer = 12;
+        System.out.println("angka : " + nomer);
+    //sesudah di update
+        nomer = 13;
+        System.out.println("angka : " + nomer);
+        
+        byte angka = 14;
+        System.out.println("nomer : " + angka);
+        angka = 15;
+        System.out.println("nomer : " + angka);
+        
+        short tahun = 2005;
+        System.out.println("Tahun : " + tahun);
+        tahun = 2006;
+        System.out.println("Tahun : " + tahun);
+        
+        long hitung = 16;
+        System.out.println("HITUNG : " + hitung);
+        hitung = 17;
+        System.out.println("HITUNG : " + hitung);
+        
+        float bilangan2 = (float) 1.70;
+        System.out.println("kg : " + bilangan2);
+        bilangan2 = (float) 2.60;
+        System.out.println("kg : " + bilangan2);
+        
+        double panjang2 = 1.70;
+        System.out.println("tinggi : " + panjang2 + 'M');
+        panjang2 = 1.80;
+        System.out.println("tinggi : " + panjang2 + 'M');
+        
+        char variabel = 'S';
+        System.out.println("kapital : " + variabel);
+        variabel = 'L';
+        System.out.println("kapital : " + variabel);
+        
+        boolean sakit = true;
+        System.out.println("sakit : " + sakit);
+        sakit = false;
+        System.out.println("sakit : " + sakit);
+        
+        String nama2 = "Muhammad fiqri";
+        System.out.println("Nama : " + nama2);
+        nama2 = "ikki";
+        System.out.println("Nama : " + nama2);
+    }
+    
+}
