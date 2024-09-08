45ffce4160ab784c93094285ff386dff0df05f25
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-08 08:37:05 +0800

    variabel inisialisasi

---
+++ Days007.java
@@ -0,0 +1,21 @@
+
+package days007.java;
+
+public class Days007Java {
+
+    public static void main(String[] args) {
+        System.out.println("conto tipe daata variabel inisialisasi");
+        // contoh tipe data nama_variabel = isi_variabel
+      
+        String nama = "Muhammad Fiqri";
+        int angka = 19;
+        byte nomor = 12;
+        short tahun = 2005;
+        long tanggal = 01;
+        float berat = (float) 45.5;
+        double panjang = 1.60;
+        char gender = 'L';
+        boolean sehat = true;
+    }
+    
+}
