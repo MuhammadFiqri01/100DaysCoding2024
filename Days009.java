1c174b001cd35220d0a4b5d656f43d40b42c271f
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-10 06:24:30 +0800

    final variabel

---
+++ Days009.java
@@ -0,0 +1,30 @@
+
+package days009.java;
+
+public class Days009Java {
+
+    public static void main(String[] args) {
+        // contoh final variabel 
+         final int nomer = 12;
+        System.out.println("angka : " + nomer);
+        /*nomer = 13;
+        System.out.println("angka : " + nomer);*/
+        
+        final byte satuan = 14;
+        System.out.println("nomer : " + satuan);
+        /*satuan = 15;
+        System.out.println("nomer : " + satuan);*/
+        
+        final short bilangan = 2005;
+        System.out.println("Tahun : " + bilangan);
+        /*bilangan = 2006;
+        System.out.println("Tahun : " + bilangan);*/
+        
+        /*final varibel adalah untuk membuat 
+        nilai pada variabel yang tidak bisa di ubah 
+        dan untuk tipe data semua bisa digunakan*/
+        
+       
+    }
+    
+}
