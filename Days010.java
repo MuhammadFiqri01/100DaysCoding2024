0c556aa6f62842eefb758d8bf5507b0cf373a3e2
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-11 07:26:37 +0800

    tipe data byte

---
+++ Days010.java
@@ -0,0 +1,12 @@
+
+package days010.java;
+
+public class Days010Java {
+
+    public static void main(String[] args) {
+        // contoh tipe data byte
+        byte jumlah = 7;
+        System.out.println("Jumlah baju : " + jumlah );
+    }
+    
+}
