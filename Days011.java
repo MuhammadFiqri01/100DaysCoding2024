852b3c99615bb76de92993552dbdc3f6c28208e5
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-12 08:55:08 +0800

    tipe data short

---
+++ Days011.java
@@ -0,0 +1,12 @@
+
+package days011.java;
+
+public class Days011Java {
+
+    public static void main(String[] args) {
+        // Contoh tipe data short
+       short Tahun = 2005;
+        System.out.println("Tahun lahir : " + Tahun);
+    }
+    
+}
