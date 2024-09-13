dec2421a63e03d0a1793e856f42987ee49bd7c8b
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-13 08:45:08 +0800

    tipe data integer

---
+++ Days012.java
@@ -0,0 +1,12 @@
+
+package days012.java;
+
+public class Days012Java {
+
+    public static void main(String[] args) {
+        // contoh tipedata integer cara pemanggilannya yaitu
+        int umur = 19;
+        System.out.println("Umur : " + umur);
+    }
+    
+}
