fc9a5e94d1ca78cc8b6da17c781230c906b58a81
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-15 10:24:01 +0800

    tipe data long

---
+++ Days014.java
@@ -0,0 +1,17 @@
+
+package days014.java;
+
+public class Days014Java {
+
+    public static void main(String[] args) {
+        //contoh tipe data  long 
+        long tahun = 2005;
+        System.out.println("Tahun lahir = " + tahun);
+        
+        /*Tipe data ini adalah salah satu tipedata primitif bilangan bulat
+        yang dimana tipe data long merupakan tipe data yang cukup banyak menampung 
+        nilai di bandingkan tipe data bilangan bulat lainnya*/
+       
+    }
+    
+}
