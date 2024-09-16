8a48d4c3978e9633fb1d3ce0ad6c0248c8321280
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-16 10:39:51 +0800

    tipe data double

---
+++ Days015.java
@@ -0,0 +1,14 @@
+
+package Days015;
+
+public class Days015 {
+    public static void main(String[] args) {
+        // contoh tipe data double
+        double panjang = 15.1;
+        System.out.println("Panjang = " + panjang + " cm");
+        
+        /* tipedata double di gunakan untuk menyimpan nilai desimal dengan 
+    presisi ganda */
+    }
+    
+}
