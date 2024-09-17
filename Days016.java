7507494c380b59ece983a9bfd1d8fbed05d9e00d
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-17 13:28:02 +0800

    tipe data float

---
+++ Days016.java
@@ -0,0 +1,17 @@
+
+package Days016;
+
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data float
+        float bilangan =  (float) 12.2;
+        System.out.println("Tinggi = " + bilangan + " M");
+        
+        /*tipe data float adalah salah satu tipe data primitif untuk menyimpan 
+        nilai desimal sama seperti double yang membedakan adalah nilai yang di tampung
+        oleh masing masing tipe data */
+        
+    }
+    
+}
