4a1281d806070c292f2e959ad83bfb30b39feb02
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-18 11:31:06 +0800

    tipe data char

---
+++ Days017.java
@@ -0,0 +1,15 @@
+
+package Dasy017;
+
+public class java { 
+    public static void main(String[] args) {
+        //contoh tipe data char
+        char huruf ='A';
+        System.out.println("Golongan darah = " + huruf);
+        
+        /* tipe data char adalah tipe data primitif yang hanya menampung satu karakter / satu huruf
+        dimana ketika lebih dari satu karakter atau huruf jadinya akan eror.
+        */
+    }
+    
+}
