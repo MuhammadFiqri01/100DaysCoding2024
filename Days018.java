5493340e7afa218ed9ad9a1ea3af56f45a4c70d9
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-19 18:12:22 +0800

    tipe data boolean

---
+++ Days018.java
@@ -0,0 +1,15 @@
+
+package days018.java;
+
+public class Days018Java {
+
+    public static void main(String[] args) {
+        // contoh tipe data boolean
+        boolean sakit = true;
+        System.out.println("Keterangan = " + sakit);
+        
+        /* tipe data boolean adalah salah satuh tipe data primitif yang hanya menampung
+        dua nilai ya itu true dan false */
+    }
+    
+}
