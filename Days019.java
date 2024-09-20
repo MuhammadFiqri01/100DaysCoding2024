8526c1e5a2c2a5b36766da8641d02c085012caa7
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-20 18:27:38 +0800

    tipe data refence String

---
+++ Days019.java
@@ -0,0 +1,16 @@
+
+package days019.java;
+
+public class Days019Java {
+
+  
+    public static void main(String[] args) {
+        // contoh tipe data refence string
+        String nama = "ikki";
+        System.out.println("Nama = " + nama);
+        
+        /*Tipe data reference string adalah tipe data non primitif ,
+        tipe data ini menampung nilain ya itu kalimat yang di bungkus oleh petik dua */
+    }
+    
+}
