9987039786c4e521266279d30a867a1ad0ec3efe
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-21 12:49:36 +0800

    tipe data reference Byte

---
+++ Days020.java
@@ -0,0 +1,16 @@
+
+package days020.java;
+
+public class Days020Java {
+
+    public static void main(String[] args) {
+        // contoh tipe data reference Byte
+        Byte angka = 19;
+        System.out.println("Umur = " + angka);
+        
+        /* tipe data reference Byte adalah tipe data non primitif nah yang membedakan
+        dengan tipe data primitif yaitu peletakan huruf kapital di awal kata sedangkan primitif 
+        menggunakan huruf kecil */
+    }
+    
+}
