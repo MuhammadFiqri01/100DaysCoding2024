883f9a3f15c436bf416104785c17f1bbb0b06ec2
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-22 14:22:46 +0800

    tipe data refence Short

---
+++ Days021.java
@@ -0,0 +1,13 @@
+
+package Dasy021;
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data reference Short
+        Short tahun = 2005;
+        System.out.println("Tahun lahir = " + tahun);
+        
+        /* tipe data reference Short adalah salah satu tipe data non primitif
+        sama seperti Byte kemarin */
+    }
+}
