2979ed8245816d225c816a310ca4b1cf30eecc6a
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-23 14:26:09 +0800

    tipe data refence Integer

---
+++ Days022.java
@@ -0,0 +1,16 @@
+
+package days022.java;
+
+public class Days022Java {
+
+ 
+    public static void main(String[] args) {
+        // contoh tipe data refence Integer
+        Integer angka = 19;
+        System.out.println("Jumlah barang = " + angka );
+        
+        /*tipe data refence Integer adalah tipe data non primitif yang beda dari tipe data primitif
+        ya itu kalau tipe data non primitif tulisan katnya Integer kalau primitif itu int */
+    }
+    
+}
