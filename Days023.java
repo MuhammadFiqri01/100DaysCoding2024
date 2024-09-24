064f5efb39a8d945b658fd03b7bceb253a560061
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-24 17:51:45 +0800

    tipe data reference Long

---
+++ Days023.java
@@ -0,0 +1,13 @@
+
+package days023.java;
+
+public class Days023Java {
+
+    public static void main(String[] args) {
+        // contoh tipe data refence Long
+        Long angka = 2005L;
+        System.out.println("Tahun lahir = " + angka);
+        /*tipe data referen long juga salah satu tipe data non primitif*/
+    }
+    
+}
