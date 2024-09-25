61c98d0e2df7b4523edb7f831e181c03181f31b5
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-25 18:43:09 +0800

    tipe data reference Float

---
+++ Dasy024.java
@@ -0,0 +1,12 @@
+
+package Dasy024;
+
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data reference Float
+        Float bb = 42.2F;
+        System.out.println("Berat badan = " + bb);
+       
+    }
+}
