e7a326984d533f5a6e7b830cab0c999efd241771
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-26 19:33:42 +0800

    tipe data reference Double

---
+++ Dasy025.java
@@ -0,0 +1,12 @@
+
+package Dasy025;
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data reference Double
+        Double tinggi = 160.5;
+        System.out.println("Tinggi badan = " + tinggi);
+        /* tipe data ini adalah tipe data non primitif untuk bilangan berkoma,*/
+    }
+    
+}
