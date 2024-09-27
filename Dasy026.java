01f30c08cf4a18a746ed8423e71e80bc2382d3f6
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-27 19:03:10 +0800

    tipe data reference Character

---
+++ Dasy026.java
@@ -0,0 +1,14 @@
+package days026;
+
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data reference Charakter
+        Character darah = 'A';
+        System.out.println("Golongan darah = " + darah);
+        
+        /*tipe data ini adalah tipe data non primitif penulisan katanya itu character,
+        */
+    }
+    
+}
