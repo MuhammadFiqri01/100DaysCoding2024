9522bf3ca169693b1e0480b8aec3a69e569631e9
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-29 19:45:47 +0800

    tipe data reference Boolean

---
+++ Dasy028.java
@@ -0,0 +1,13 @@
+
+package Days028;
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data reference Boolean
+        Boolean sakit = true;
+        System.out.println("sehat = " + sakit);
+        /*tipe data ini adalah tipe data non primitif penulisan katanya di awali
+        oleh huruf besar yaitu Boolean*/
+    }
+    
+}
