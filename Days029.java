e8964eeeadb51d9f7925247a548880c3b5bced75
MuhammadFiqri01 <fikki659@gmail.com>
2024-09-30 21:04:57 +0800

    tipe data reference BigInteger

---
+++ Days029.java
@@ -0,0 +1,16 @@
+
+package days029;
+import java.math.BigInteger;
+
+public class java {
+    public static void main(String[] args) {
+        //contoh tipe data reference BigInteger
+        BigInteger input = new BigInteger("2005");
+        System.out.println("tahun lahir = " + input);
+        /*tipe data ini adalah tipe data non primitif yang menggunakan import java.math.BigInteger
+        titik koma, cara pemanggilannya yaitu BigInteger namavariabel yaitu input = new BigInteger
+        dalam kurung 2005 dibungkus oleh petik dua */
+        
+    }
+    
+}
