dde81a19ada467bdebc9b86a905d7ee7d4bc5126
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-01 20:32:20 +0800

    tipe data reference BigDecimal

---
+++ Dasy030.java
@@ -0,0 +1,15 @@
+
+package dasy030.java;
+import java.math.BigDecimal;
+public class Dasy030Java {
+
+ 
+    public static void main(String[] args) {
+        // contoh tipe data reference BigDecimal
+        BigDecimal BB = new BigDecimal(45.5);
+        System.out.println("Berat badan = " + BB);
+        /*tipe data ini adalah tipe data non primitif yang menampung nilai berkoma
+        dan juga menggunakan import untuk menggunakan tipe data ini*/
+    }
+    
+}
