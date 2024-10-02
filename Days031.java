fa8873b3a09cd0967e0eb1932ca8de2c680ea993
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-02 20:12:47 +0800

    konversi String ke tipedata primitif

---
+++ Days031.java
@@ -0,0 +1,16 @@
+
+package days031;
+
+
+public class java {
+    public static void main(String[] args) {
+        //konversi String ke tipe data primitif
+        String a = "13";
+        int b = Integer.parseInt(a);
+        System.out.println(b);
+        
+        /* tipe data ini adalah untuk mengkonversi tipe data ke primitif seperti string */
+     
+    }
+    
+}
