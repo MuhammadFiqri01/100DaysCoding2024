165e8ab429c96516e2128cd41812205c40ef26a2
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-03 19:16:01 +0800

    konversi tipe data primitif ke  string

---
+++ Days032.java
@@ -0,0 +1,20 @@
+
+package days032;
+
+
+public class java {
+    public static void main(String[] args) {
+        // contoh konversi tipe data primitif ke String
+        /*tipe data konversi ini adalah untuk mengkonversi tipe data primitif ke String
+        contoh nya seperti tipe data di bawah ini*/
+        int A = 12;
+        String B = A + "";
+        System.out.println(B);
+        byte a =13;
+        String b = a + "";
+        System.out.println(b);
+        
+     
+    }
+    
+}
