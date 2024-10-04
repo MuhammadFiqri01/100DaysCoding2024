1f938bdd89d63177b369bdc0c92fbc34b7fd9fa0
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-04 19:23:14 +0800

    type Casting

---
+++ Days033.java
@@ -0,0 +1,17 @@
+public class java {
+    public static void main(String[] args) {
+        //contoh type Casting
+         /* tipe casting ini adalah untuk mengubah tipe data satu ke tipe data lainnya*/ 
+        int a = 12;
+        byte b = (byte) a;
+        short c = 13;
+        long d = c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+      
+        
+    }
+    
+}
