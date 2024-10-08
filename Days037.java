2d0f106c9fa24103cae3b7a63138a1ade96ab352
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-08 20:16:38 +0800

    penugasan penjumlahan dan pengurangan

---
+++ Days037.java
@@ -0,0 +1,15 @@
+
+package days037;
+
+public class java {
+    public static void main(String[] args) {
+        //penugasan 
+         int a = 12;
+         a += 12;
+         byte b = 13;
+         b -= 3;
+         System.out.printf("hasil penjumlahan variabel a adalah %d dan hasil pengurangan variabel b adalah %d  ",a,b);
+      
+    }
+    
+}
