8cd19744f3a8c2403a38ec3c37f8f3a1be9c399f
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-09 19:02:30 +0800

    operator penugasan perkalian dan pembagian

---
+++ Days038.java
@@ -0,0 +1,15 @@
+
+package days038;
+
+
+public class java {
+    public static void main(String[] args) {
+        //operator penugasan perkalian dan pembagian
+        int a = 12;
+        byte b = 13;
+        a *=3;
+        b /=3;
+        System.out.printf("hasil dari variabel a adalah %d dan hasil dari variabel b adalah %d",a,b);
+    }
+    
+}
