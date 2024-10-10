50dea93b770b53c2b0ec83cf2c6532e4b4edcaef
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-10 22:21:21 +0800

    pengisisn dan modulo

---
+++ Days039.java
@@ -0,0 +1,15 @@
+
+package days039;
+
+
+public class java {
+    public static void main(String[] args) {
+        //pengisian dan modulo
+        int a = 12;
+        a %= 3;
+        int b = 9;
+        b %= 2;
+        System.out.printf("hasil variabrl a adalah %d dan hasil variabel b adalah %d ",a,b);
+    }
+    
+}
