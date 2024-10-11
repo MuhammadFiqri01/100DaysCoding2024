ca1a356e9ddb7f0b133235c0277a4eb2ee824948
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-11 16:03:23 +0800

    operator perbandingan lebih besar dan lebih kecil

---
+++ Days040.java
@@ -0,0 +1,15 @@
+
+package days040;
+
+public class java {
+    public static void main(String[] args) {
+        //operator perbandingan lebih besar dan lebih kecil
+        int a = 12;
+        int b = 13;
+        boolean c = a > b;
+        boolean d = a < b;
+        System.out.println("hasil dari a > b adalah " + c);
+        System.out.println("hasil dari a < b adalah " + d);
+    }
+    
+}
