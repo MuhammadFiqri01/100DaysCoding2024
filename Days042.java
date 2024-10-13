48c5aedabb6698ff93bb658a710dd46b3c11277d
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-13 15:31:43 +0800

    operator perbandingan sama dengan dan tidak sama dengan

---
+++ Days042.java
@@ -0,0 +1,15 @@
+
+package days042;
+
+
+public class java {
+    public static void main(String[] args) {
+        // operator perbandingan sama dengan dan tidak sama dengan
+        
+        byte a = 12;
+        int b = 12;
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+    
+}
