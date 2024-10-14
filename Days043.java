42e15a277e59c99ee9fd8276fa63aacb9314e6e1
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-14 18:49:30 +0800

    operator penugasan >= dan <=

---
+++ Days043.java
@@ -0,0 +1,16 @@
+
+package days043.java;
+
+
+public class Days043Java {
+
+  
+    public static void main(String[] args) {
+        // operator penugasan >= dan <=
+        int a = 12;
+        int b = 14;
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+    
+}
