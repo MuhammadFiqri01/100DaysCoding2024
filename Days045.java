81deb498ed1d19267ce550be1f82523a1a4fc446
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-16 00:12:32 +0800

    operator penugasan or

---
+++ Days045.java
@@ -0,0 +1,14 @@
+
+package days045;
+
+
+public class java {
+    public static void main(String[] args) {
+        // operator logika or
+        int a = 12;
+        int b = 5;
+        int c = 9;
+        System.out.println(a < b || a > c);
+    }
+    
+}
