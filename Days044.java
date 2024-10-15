9047ff15d165b6b39ab00b5485e7d83e9e495318
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-15 22:17:47 +0800

    operator logika and

---
+++ Days044.java
@@ -0,0 +1,14 @@
+
+package days044;
+
+
+public class java {
+    public static void main(String[] args) {
+        // operator logika dan
+        int a = 12;
+        int b = 13;
+        int c = 14;
+        System.out.println(a < b && a > c);
+    }
+    
+}
