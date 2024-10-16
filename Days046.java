a9180bd95cb6a0888d161a32e4ac61d99e69cd9b
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-17 01:30:51 +0800

    operator logika not

---
+++ Days046.java
@@ -0,0 +1,15 @@
+
+package days046;
+
+
+public class java {
+    public static void main(String[] args) {
+        // operator logika Not
+        int a = 12;
+        int b = 13;
+        boolean c = ! ( a > b );
+        System.out.println(c);
+        
+    }
+    
+}
