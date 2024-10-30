dde4e40bbba70ca13668969570f58af2fc20175b
MuhammadFiqri01 <fikki659@gmail.com>
2024-10-30 19:04:52 +0800

    looping for each

---
+++ Days059.java
@@ -0,0 +1,15 @@
+
+package days059;
+
+
+public class java {
+    public static void main(String[] args) {
+        // looping for Each
+       int a [] = { 1,2,3,4,5 };
+        for (int i : a ) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+    
+}
