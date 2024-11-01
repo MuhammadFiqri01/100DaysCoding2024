997f227ec1f727f9dc4c796bc8b0651c5bbd6b8a
MuhammadFiqri01 <fikki659@gmail.com>
2024-11-01 14:19:39 +0800

    infinite loop

---
+++ Days061.java
@@ -0,0 +1,12 @@
+
+package days061;
+
+public class java {
+    public static void main(String[] args) {
+        //Infinite Loop
+        while (true) {
+            System.out.println("MUHAMMAD FIQRI");
+        }
+    }
+    
+}
