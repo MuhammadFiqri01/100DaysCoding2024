2b965174a5925948e457d882e8578fbb3a7277e9
MuhammadFiqri01 <fikki659@gmail.com>
2024-11-08 19:56:54 +0800

    method non-void

---
+++ Days068.java
@@ -0,0 +1,13 @@
+
+package days068;
+public class java {
+     public int tambah(int a, int b) {
+         // Method Non-void
+        return a + b;
+    }
+     public static void main(String[] args) {
+        java b = new java ( );
+      int a = b.tambah(2, 5);
+         System.out.println(a);
+     }
+}
