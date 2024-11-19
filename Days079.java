0a6eb6a0097535144ae69852ebe518e379b97c98
MuhammadFiqri01 <fikki659@gmail.com>
2024-11-19 19:03:11 +0800

    String Method  toString()

---
+++ Days079.java
@@ -0,0 +1,13 @@
+
+package days079;
+public class java {
+     public static void main(String[] args) {
+         //String Method toString()
+
+        Object a = "MUHAMMAD FIQRI";
+
+        String b = a.toString();
+
+        System.out.println("Hasil toString() = " + b);
+    }
+}
