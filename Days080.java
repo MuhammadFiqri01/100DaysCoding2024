560b75266fb7a87deaa1efe77b01c6619c4174aa
MuhammadFiqri01 <fikki659@gmail.com>
2024-11-20 19:28:17 +0800

    String Method  trim()

---
+++ Days080.java
@@ -0,0 +1,10 @@
+package days080;
+public class java {
+    public static void main(String[] args) {
+        //String Method : trim()
+        String a = "    MUHAMMAD FIQRI    ";
+        System.out.println("Sebelum trim = '" + a + "'");
+        String b = a.trim();  
+        System.out.println("Setelah trim = '" + b + "'");
+    }   
+}
