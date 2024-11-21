51387eee631f1d702e97bed0df1a8e95bfc2e406
MuhammadFiqri01 <fikki659@gmail.com>
2024-11-21 20:57:30 +0800

    String Method  valueOf()

---
+++ Days081.java
@@ -0,0 +1,11 @@
+
+package days081;
+public class java {
+    public static void main(String[] args) {
+        //String Method : valueOf()
+        int a = 100;
+        String b = String.valueOf(a);
+        System.out.println("Angka sebagai String = " + b);
+       
+    }  
+}
